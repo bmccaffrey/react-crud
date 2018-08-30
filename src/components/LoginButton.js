620715b587870cd2357f067d867dbd73fde1c1a7
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-30 09:34:27 -0500

    import Account Circle icon

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -7,6 +7,10 @@ import {
   ListItemText
 } from '@material-ui/core';
 
+// Can't compile with this import, but I'm following the docs to the letter
+// Looks like it has incorrect peer dependencies
+import { AccountCircle } from '@material-ui/icons';
+
 export default class LoginButton extends Component {
   render() {
     return <div>TEST</div>;
