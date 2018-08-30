839eff68c0a7d9ae8a61d47767c852cd84d871e2
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-30 08:30:22 -0500

    import BrowserRouter & Security

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Security } from '@okta/okta-react';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
