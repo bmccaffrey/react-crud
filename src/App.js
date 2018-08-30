ed91c5ed7bfbb2bb0df8d1b6d250fe9235ca37b8
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-30 07:46:42 -0500

    replace boilerplate

--- src/App.js
+++ src/App.js
@@ -3,20 +3,12 @@ import { CssBaseline, withStyles } from '@material-ui/core';
 import AppHeader from './components/AppHeader';
 import Home from './pages/Home';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = ({ classes }) => (
+  <Fragment>
+    <CssBaseline />
+    <AppHeader />
+    <Home />
+  </Fragment>
+);
 
 export default App;
