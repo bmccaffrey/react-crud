b0e9f77dafc1615f339db5e4504fd92a586a4764
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-30 08:51:10 -0500

    import components from Material-UI

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import {
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+  ListItemText
+} from '@material-ui/core';
 
 export default class LoginButton extends Component {
   render() {
