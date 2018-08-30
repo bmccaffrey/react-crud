0c19daa6d521cee5656f3397dd01e48f4176ddb1
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-30 07:50:20 -0500

    define main style with JSS

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import { CssBaseline, withStyles } from '@material-ui/core';
 import AppHeader from './components/AppHeader';
 import Home from './pages/Home';
 
+const styles = theme => ({
+  main: {
+    padding: 3 * theme.spacing.unit,
+    [theme.breakpoints.down('xs')]: {
+      padding: 2 * theme.spacing.unit
+    }
+  }
+});
+
 const App = ({ classes }) => (
   <Fragment>
     <CssBaseline />
