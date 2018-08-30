d0413257350d27917b47b85a5f908e90f0d43b2c
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-30 07:44:56 -0500

    add imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { CssBaseline, withStyles } from '@material-ui/core';
+import AppHeader from './components/AppHeader';
+import Home from './pages/Home';
 
 class App extends Component {
   render() {
