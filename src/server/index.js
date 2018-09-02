cd223491d136137167df8042b739f287e5f5da20
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-09-01 20:32:38 -0500

    add express boilerplate

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,14 @@
 require('dotenv').config({ path: '.env.local' });
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const Sequelize = require('sequelize');
 const epilogue = require('epilogue');
 const OktaJwtVerifier = require('@okta/jwt-verifier');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Test'));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
