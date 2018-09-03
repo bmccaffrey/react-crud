079bb3564e0c77014d060bf9ec653d37d5b0b244
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-09-03 12:25:15 -0500

    define async fetch function

--- src/pages/PostsManager.js
+++ src/pages/PostsManager.js
@@ -40,6 +40,23 @@ export default class PostsManager extends Component {
     posts: []
   };
 
+  async fetch(method, endpoint, body) {
+    try {
+      const response = await fetch(`${API}${endpoint}`, {
+        method,
+        body: body && JSON.stringify(body),
+        headers: {
+          'content-type': 'application/json',
+          accept: 'application/json',
+          authorization: `Bearer ${await this.props.auth.getAccessToken()}`
+        }
+      });
+      return await response.json();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   render() {
     const { classes } = this.props;
 
