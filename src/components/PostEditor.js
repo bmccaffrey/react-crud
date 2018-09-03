b2639e21617f358b07d12ca3911208b02db7d5d3
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-09-03 11:17:13 -0500

    define styles

--- src/components/PostEditor.js
+++ src/components/PostEditor.js
@@ -12,6 +12,25 @@ import { compose } from 'recompose';
 import { withRouter } from 'react-router-dom';
 import { Form, Field } from 'react-final-form';
 
+const styles = theme => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  modalCard: {
+    width: '90%',
+    maxWidth: 500
+  },
+  modalCardContent: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  marginTop: {
+    marginTop: 2 * theme.spacing.unit
+  }
+});
+
 const PostEditor = () => (
   <Form>
     <Modal>
